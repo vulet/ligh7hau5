402594f109ea77b35b3747430de6a6a9eb73f765
vulet <spghtz@gmail.com>
2020-06-28 15:27:50 +0800

    chore(docs): tidy up

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,8 @@
 exports.runQuery = function (matrixClient, room) {
   matrixClient.sendHtmlNotice(room.roomId,
     '',
-    '<blockquote><b>+plemara [your message] : post<br>'
+    '<blockquote><b>fediverse commands<br>'
+          + '+plemara [your message] : post<br>'
           + '+redact [post id] : delete post<br>'
           + '+fren [user id] : follow<br>'
           + '+unfren [user id] : unfollow<br>'
@@ -13,12 +14,12 @@ exports.runQuery = function (matrixClient, room) {
           + '+boo [post id] : unfavorite</blockquote>'
           + '<blockquote><b>channel commands<br>'
           + '+flood : turn on timeline in channel<br>'
-          + '+notify : show notifications in channel</b>'
+          + '+notify : show notifications in channel<br>'
           + '+archive [URL] : archive content<br>'
           + '+rearchive [URL] : re-archive content<br>'
           + '+nitter [status URL] : redirect twitter to nitter, also embed tweet<br>'
-          + '+invidious [video URL] : redirect youtube to invidious, also embed description<br></blockquote>'
-          + '<blockquote><b>--- <i>contributors🐱</i> ---</b>'
-          + '--- <i>cryptomooners</i> ---</blockquote>'
-          + '<blockquote><b>--- <i>docs by lint</i> ---</b></blockquote>');
+          + '+invidious [video URL] : redirect youtube to invidious, also embed description</b><br></blockquote>'
+          + '<blockquote><b>--- <i>Contributors🐱</i> ---</b><br>'
+          + '<b>CRYPTOMOONERS</b><br>'
+          + '<b><i>docs by LINT</i></b></blockquote>');
 };
