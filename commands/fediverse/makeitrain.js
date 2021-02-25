f37e2471c0a52a19e8226b168bf2a76a26ab4da7
vulet <spghtz@gmail.com>
2021-02-25 16:43:08 +0800

    fix(10grans): forecast regression

--- commands/fediverse/makeitrain.js
+++ commands/fediverse/makeitrain.js
@@ -4,7 +4,7 @@ exports.runQuery = async function (roomId, event, notice) {
   const original = await matrixClient.sendHtmlNotice(roomId, `${loadingString}`, `<code>${loadingString}</code>`);
   const findAllUsers = await fediverse.utils.getStatusMentions(notice, event, original);
   const exclude = "@10grans@fedi.cc";
-  const users = users.filter(users => !exclude.includes(findAllUsers))
+  const users = findAllUsers.filter(users => !exclude.includes(findAllUsers))
   if (!users) return matrix.utils.editNoticeHTML(roomId, original, `<code>No eligible users found.</code>`);
   const rain = (users) => {
     amount = users.length * 0.00000001337 // by per user:
